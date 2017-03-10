16c2ef79e3a6e0932c9065428000d17814801b39
batdongsan3c <batdongsan3c001@gmail.com>
2017-03-10 15:41:03 +0700

    Update multitab.js

--- multitab.js
+++ multitab.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function() {
         var currentAttrValue = jQuery(this).attr('href');
  
         // Show/Hide Tabs
-        jQuery(this).parent(currentAttrValue).addClass('active').siblings().removeClass('active');
+        jQuery('.tab-content ' + currentAttrValue).show().siblings().hide();
  
         // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
